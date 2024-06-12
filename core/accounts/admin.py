44fef1a3156db2eca8d70de90fd6529dfe5e3c1d
barbod imani <barbodimani8181@gmail.com>
2024-06-13 01:18:47 +0330

    user admin

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -6,3 +6,20 @@ from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        ("Persmissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_active", "is_superuser")
+        }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
