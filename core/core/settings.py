610b3fd1b85affa91a61b3f2f3df4b65e5d8910a
barbod imani <barbodimani8181@gmail.com>
2024-06-13 00:27:41 +0330

    accounts

--- core/core/settings.py
+++ core/core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     # my apps:
     "accounts",
-    "blog",
+    # "blog",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +126,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.User"
